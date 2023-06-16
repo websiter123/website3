34412f35ec94e8f8d55f65c886c015066848e749
websiter123 <136844990+websiter123@users.noreply.github.com>
2023-06-17 00:22:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Enable CORS middleware
 app.use((req, res, next) => {
